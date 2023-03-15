df32d19872a9bb78d16d96f397136720a7824d4f
PavelsKonins <onetwolv@gmail.com>
2023-03-15 19:57:06 +0200

    classroom_06032023: Add classroom progress

---
+++ src/main/java/lv/acodemy/classroom/cat.java
@@ -0,0 +1,19 @@
+package lv.acodemy.classroom;
+
+class Cat {
+    public String name;
+
+    public String color;
+
+    public double weight;
+
+    public String breed;
+
+    public String gender;
+
+    public boolean hasFur;
+
+
+
+}
+
